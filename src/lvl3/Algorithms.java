181b3f1fb99c84077353e17f66515d36e417e1c4
James Maron <jamesmaron87@gmail.com>
2015-07-15 18:27:26 -0700

    djhsajdsafdais
    saufeftytYUtyufuytsadtffstadFTADftda

---
+++ src/lvl3/Algorithms.java
@@ -0,0 +1,12 @@
+package lvl3;
+
+import java.util.ArrayList;
+
+public class Algorithms {
+private void findBrokenEgg(ArrayList eggs) {
+	for (Object s : eggs) {
+	
+	}
+
+}
+}
--- src/lvl3/pluse1.java
+++ src/lvl3/pluse1.java
@@ -1,13 +1,13 @@
 package lvl3;
 
 public class pluse1 {
-	int x = 1;
+	int x = 2147483515;
 public static void main(String[] args) {
 	pluse1 run = new pluse1();
 	run.run();
 }
 private void run() {
-	for (int i = 1; x != -1; i++) {
+	for (int i = 1; x > -1; i++) {
 		x++;
 		System.out.println(x + " line " + i );
 	}
